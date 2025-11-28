7eb1a260c1b2cec62daaf1bc43b7a9cded8ae7a1
MishaP <MishaP635@gmail.com>
2025-11-28 09:20:02 +0200

    first commit

---
+++ script2.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("myBtn")
+const text  = document.getElementById("text")
+
+
+btn.addEventListener("click", function(){
+
+    text.innerText = "Ви натискли кнопку"
+} )Передача некоректного значення style
